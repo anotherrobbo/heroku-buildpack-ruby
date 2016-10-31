88a1d9d873ea641471054284c998dc2f13fbefeb
anotherrobbo <littlejerry7@hotmail.com>
2016-10-31 15:17:11 +1030

    more logging

--- lib/language_pack/sql_lite.rb
+++ lib/language_pack/sql_lite.rb
@@ -5,7 +5,7 @@ class LanguagePack::SqlLite < LanguagePack::Base
   def self.use?
     pwd = Dir.pwd
     bps = "#{pwd}/heroku_buildpack_scripts"
-    info "Checking for #{bps}/doextra.sh"
+    puts "Checking for #{bps}/doextra.sh"
     # If #{bps}/doextra.sh exists, then run this.
     File.exists?("#{bps}/doextra.sh")
   end
@@ -18,6 +18,7 @@ class LanguagePack::SqlLite < LanguagePack::Base
     # Here we do the actual setup for SqlLite
     pwd = Dir.pwd
     bps = "#{pwd}/heroku_buildpack_scripts"
+    puts "Calling #{bps}/doextra.sh"
     cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
     cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
     cmd3 = "/bin/bash #{hbs}/dothis.bash"
