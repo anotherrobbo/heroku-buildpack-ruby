3e0e0fc591cd1e13f9e0d964ab208f2a441bda6b
anotherrobbo <littlejerry7@hotmail.com>
2016-10-31 15:24:23 +1030

    Change to do_extra

--- lib/language_pack/do_extra.rb
+++ lib/language_pack/do_extra.rb
@@ -1,7 +1,7 @@
 require "language_pack"
 require "language_pack/base"
 
-class LanguagePack::SqlLite < LanguagePack::Base
+class LanguagePack::DoExtra < LanguagePack::Base
   def self.use?
     pwd = Dir.pwd
     bps = "#{pwd}/buildpack_scripts"
@@ -11,17 +11,17 @@ class LanguagePack::SqlLite < LanguagePack::Base
   end
 
   def name
-    "Ruby/SqlLite"
+    "Ruby/DoExtra"
   end
 
   def compile
-    # Here we do the actual setup for SqlLite
+    # Here we do the actual extra script running
     pwd = Dir.pwd
     bps = "#{pwd}/buildpack_scripts"
     puts "Calling #{bps}/doextra.sh"
     cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
     cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
-    cmd3 = "/bin/bash #{hbs}/dothis.bash"
+    cmd3 = "/bin/bash #{bps}/doextra.sh"
     cmds = "#{cmd1} #{cmd2} #{cmd3} "
     bundler_output << pipe("cmds")
   end
