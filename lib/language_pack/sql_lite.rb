9fb1af4bd021928ec717f01614915219ca0ae155
anotherrobbo <littlejerry7@hotmail.com>
2016-10-31 15:03:44 +1030

    mock start

---
+++ lib/language_pack/sql_lite.rb
@@ -0,0 +1,24 @@
+require "language_pack"
+require "language_pack/base"
+
+class LanguagePack::SqlLite < LanguagePack::Base
+  def self.use?
+    bps = "#{pwd}/heroku_buildpack_scripts"
+    # If #{bps}/doextra.sh exists, then run this.
+    File.exists?("#{bps}/doextra.sh")
+  end
+
+  def name
+    "Ruby/SqlLite"
+  end
+
+  def compile
+    # Here we do the actual setup for SqlLite
+    bps = "#{pwd}/heroku_buildpack_scripts"
+    cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
+    cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
+    cmd3 = "/bin/bash #{hbs}/dothis.bash"
+    cmds = "#{cmd1} #{cmd2} #{cmd3} "
+    bundler_output << pipe("cmds")
+  end
+end
