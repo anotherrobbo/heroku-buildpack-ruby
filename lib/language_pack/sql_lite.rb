fea0d465f0f13c077bcde80de9eb99afe8a9086b
anotherrobbo <littlejerry7@hotmail.com>
2016-10-31 15:21:31 +1030

    fix path

--- lib/language_pack/sql_lite.rb
+++ lib/language_pack/sql_lite.rb
@@ -4,7 +4,7 @@ require "language_pack/base"
 class LanguagePack::SqlLite < LanguagePack::Base
   def self.use?
     pwd = Dir.pwd
-    bps = "#{pwd}/heroku_buildpack_scripts"
+    bps = "#{pwd}/buildpack_scripts"
     puts "Checking for #{bps}/doextra.sh"
     # If #{bps}/doextra.sh exists, then run this.
     File.exists?("#{bps}/doextra.sh")
@@ -17,7 +17,7 @@ class LanguagePack::SqlLite < LanguagePack::Base
   def compile
     # Here we do the actual setup for SqlLite
     pwd = Dir.pwd
-    bps = "#{pwd}/heroku_buildpack_scripts"
+    bps = "#{pwd}/buildpack_scripts"
     puts "Calling #{bps}/doextra.sh"
     cmd1 = "export GEM_PATH=#{ENV["GEM_PATH"]};"
     cmd2 = "export GEM_HOME=#{ENV["GEM_HOME"]};"
